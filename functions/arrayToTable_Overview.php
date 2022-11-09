ab71b7bfcfdbf8f2d5ab88a714692dea212e2771
dnitra <109307365+dnitra@users.noreply.github.com>
2022-11-09 10:39:50 +0100

    added comments

--- functions/arrayToTable_Overview.php
+++ functions/arrayToTable_Overview.php
@@ -5,45 +5,44 @@
  */
 
 
-function arrayToTable_Overview(array $arrayOfMovies){
+function arrayToTable_Overview(array $arrayOfMovies)
+{
 
     /**
      * seradit data z tabulky podle roku
      */
- usort($arrayOfMovies, function($a, $b) {
-    return ($a->yearReleased - $b->yearReleased);
- });
+    usort($arrayOfMovies, function ($a, $b) {
+        return ($a->yearReleased - $b->yearReleased);
+    });
 
 
     echo " <h2>Přehled výherců Oscaru podle roku</h2>";
     echo "<table class='table'>";
-    
+
     echo '<thead>
 
     <th scope="col">Rok</th>
     <th scope="col">Ženy</th>
     <th scope="col">Muži</th>
     </thead>';
-    
+
     echo '<tbody>';
-    foreach($arrayOfMovies as $movie){
-        
+    foreach ($arrayOfMovies as $movie) {
+
         echo "<tr>";
-           
-            echo "<td rowspan='2'>{$movie->yearReleased}</td>";
-            echo "<td>{$movie->femaleActor['actorName']} ({$movie->femaleActor['actorAge']} )</td>";
-            echo "<td>{$movie->maleActor['actorName']} ({$movie->maleActor['actorAge']} )</td>";
-        
+
+        echo "<td rowspan='2'>{$movie->yearReleased}</td>";
+        echo "<td>{$movie->femaleActor['actorName']} ({$movie->femaleActor['actorAge']} )</td>";
+        echo "<td>{$movie->maleActor['actorName']} ({$movie->maleActor['actorAge']} )</td>";
+
         echo "</tr>";
-       
+
         echo "<tr>";
-            echo "<td>{$movie->femaleActor['movieName']}</td>";
-            echo "<td>{$movie->maleActor['movieName']}</td>";
+        echo "<td>{$movie->femaleActor['movieName']}</td>";
+        echo "<td>{$movie->maleActor['movieName']}</td>";
         echo "</tr>";
-        
     }
-    
+
     echo '</tbody>';
     echo "</table>";
 }
-
